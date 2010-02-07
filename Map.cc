33ddbde637b26b976bbecde947547b54e00098a4
ebrahim <none@none>
2010-02-07 12:04:33 +0330

    Backed out changeset 7e614ca2fad7

--- Map.cc
+++ Map.cc
@@ -12,28 +12,22 @@ Map::Map()
 	read_from_file(stdin);
 }
 
+int Map::width() const
+{
+	return map_width;
+}
+
+int Map::height()  const
+{
+	return map_height;
+}
+
 bool Map::is_wall(int x, int y) const
 {
-	if (x < 0 || y < 0 || x >= width || y >= height)
+	if (x < 0 || y < 0 || x >= map_width || y >= map_height)
 		return true;
 	else
 		return map[x][y];
-}
-
-bool Map::is_wall(int move) const
-{
-	switch (move)
-	{
-		case NORTH:
-			return is_wall(player_one_x, player_one_y - 1);
-		case EAST:
-			return is_wall(player_one_x + 1, player_one_y);
-		case SOUTH:
-			return is_wall(player_one_x, player_one_y + 1);
-		case WEST:
-			return is_wall(player_one_x - 1, player_one_y);
-	}
-	return true;
 }
 
 int Map::my_x() const
@@ -65,20 +59,20 @@ void Map::make_move(int move)
 void Map::read_from_file(FILE *file_handle)
 {
 	int x, y, c;
-	int num_items = fscanf(file_handle, "%d %d\n", &width, &height);
+	int num_items = fscanf(file_handle, "%d %d\n", &map_width, &map_height);
 	if (feof(file_handle) || num_items < 2)
 		exit(0);		// End of stream means end of game. Just exit.
 	memset(map, 0, sizeof(map));
 	x = 0;
 	y = 0;
-	while (y < height && (c = fgetc(file_handle)) != EOF)
+	while (y < map_height && (c = fgetc(file_handle)) != EOF)
 	{
 		switch (c)
 		{
 			case '\r':
 				break;
 			case '\n':
-				if (x != width)
+				if (x != map_width)
 				{
 					fprintf(stderr, "x != width in Board_ReadFromStream\n");
 					return;
@@ -87,7 +81,7 @@ void Map::read_from_file(FILE *file_handle)
 				x = 0;
 				break;
 			case '#':
-				if (x >= width)
+				if (x >= map_width)
 				{
 					fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 					return;
@@ -96,7 +90,7 @@ void Map::read_from_file(FILE *file_handle)
 				++x;
 				break;
 			case ' ':
-				if (x >= width)
+				if (x >= map_width)
 				{
 					fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 					return;
@@ -105,7 +99,7 @@ void Map::read_from_file(FILE *file_handle)
 				++x;
 				break;
 			case '1':
-				if (x >= width)
+				if (x >= map_width)
 				{
 					fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 					return;
@@ -116,7 +110,7 @@ void Map::read_from_file(FILE *file_handle)
 				++x;
 				break;
 			case '2':
-				if (x >= width)
+				if (x >= map_width)
 				{
 					fprintf(stderr, "x >= width in Board_ReadFromStream\n");
 					return;
@@ -131,9 +125,4 @@ void Map::read_from_file(FILE *file_handle)
 				return;
 		}
 	}
-}
-
-bool Map::stuck() const
-{
-	return is_wall(NORTH) && is_wall(EAST) && is_wall(SOUTH) && is_wall(WEST);
 }
